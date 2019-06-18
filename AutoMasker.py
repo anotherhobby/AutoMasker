c5c755d1dd4717e0ef0f1c26611abf1c1f15885b
Dan Cunningham <dmcunningham@spscommerce.com>
2019-06-18 09:45:48 -0500

    updated display info with masked size

--- AutoMasker.py
+++ AutoMasker.py
@@ -29,8 +29,8 @@ aspects = sorted(aspect_ratios, key=lambda x: (aspect_ratios[x]['ratio']), rever
 setScreenColor(0x000000)
 title1 = M5Title(title="AutoMasker Screen Controller", x=4 , fgcolor=0xffffff, bgcolor=0x4343eb)
 label1 = M5TextBox(10, 30, "Aspect:", lcd.FONT_DejaVu18,0xFFFFFF, rotate=0)
-# label2 = M5TextBox(10, 50, "", lcd.FONT_DejaVu18,0xFFFFFF, rotate=0)
-label3 = M5TextBox(10, 50, "Last:", lcd.FONT_Ubuntu,0xFFFFFF, rotate=0)
+label2 = M5TextBox(10, 50, "Size:", lcd.FONT_Ubuntu,0xFFFFFF, rotate=0)
+label3 = M5TextBox(10, 70, "Last:", lcd.FONT_Ubuntu,0xFFFFFF, rotate=0)
 label4 = M5TextBox(10, 100, 'Masking {}"x{}" Screen'.format(screenw_in, screenh_in), lcd.FONT_DejaVu18,0xFFFFFF, rotate=0)
 label5 = M5TextBox(10, 120, "Mask Width:", lcd.FONT_Ubuntu,0xFFFFFF, rotate=0)
 label6 = M5TextBox(10, 140, "From Last:", lcd.FONT_Ubuntu,0xFFFFFF, rotate=0)
@@ -61,7 +61,7 @@ btnC.wasPressed(buttonC_wasPressed)
 def mask_width(target_w):
     return round((screenw_in - target_w)/2, 2)
 
-# calculate the wideth of the screen for agiven ratio
+# calculate the width of the screen for a given ratio
 def ratio_width(ratio):
     return round((screenh_in * ratio), 2)
 
@@ -96,6 +96,7 @@ def move_mask(aspect_ratio="anamorph24"):
     global current
     current_mask = mask_width(ratio_width(aspect_ratios[current]['ratio']))
     target_mask = mask_width(ratio_width(aspect_ratios[aspect_ratio]['ratio']))
+    new_screenw_in = ratio_width(aspect_ratios[current]['ratio'])
     target_name = aspect_ratios[aspect_ratio]['name']
     if current_mask > target_mask:
         difference = current_mask - target_mask
@@ -104,6 +105,7 @@ def move_mask(aspect_ratio="anamorph24"):
         difference = target_mask - current_mask
         direction = 'IN'
     label1.setText("Aspect: {}".format(target_name))
+    label2.setText('Size: {}"x{}" @ {}" diagonal'.format(new_screenw_in, screenh_in, diagonal(new_screenw_in, screenh_in)))
     label3.setText("Last: {}".format(aspect_ratios[current]['name']))
     label5.setText('Mask Width: {}"'.format(target_mask))
     label6.setText('From Last: {}"'.format(difference))
